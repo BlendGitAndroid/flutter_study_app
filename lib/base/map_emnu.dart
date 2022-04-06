56ef1e3eef88695fe3414a6fb501260d2c3283e9
BlendAndroid <1059152860@qq.com>
2022-04-06 20:32:21 +0800

    feat：Dart基础--map

---
+++ lib/base/map_emnu.dart
@@ -0,0 +1,15 @@
+import 'dart:convert';
+
+///map
+main() {
+  var map1 = <String, dynamic>{'name': 'zeno', 'age': 20, 'weight': 120};
+  print(map1);
+
+  //转换成json
+  var jsonStr = jsonEncode(map1);
+  print(jsonStr);
+
+  //遍历
+  map1['address'] = 'hu nan chang sha';
+  map1.forEach((key, val) => print("$key : $val"));
+}
