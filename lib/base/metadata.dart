37db8a81a6447b95d2251955ff60764f51b057f8
BlendAndroid <1059152860@qq.com>
2022-04-11 21:04:51 +0800

    feat：Dart基础--注解

---
+++ lib/base/metadata.dart
@@ -0,0 +1,31 @@
+import 'metadataCustom.dart';
+
+//注解
+main() {
+  dynamic tv = new Television();
+  tv.activate();
+  tv.turnOn();
+  tv.turnOff();
+  tv.doSomething();
+}
+
+class Television {
+  @deprecated
+  void activate() {
+    turnOn();
+  }
+
+  void turnOn() {
+    print('Television turn on!');
+  }
+
+  @override
+  noSuchMethod(Invocation mirror) {
+    print('没有找到方法');
+  }
+
+  @MetadataCustom(who: 'damon', what: 'create a new method')
+  void doSomething() {
+    print('doSomething');
+  }
+}
---
+++ lib/base/metadataCustom.dart
@@ -0,0 +1,6 @@
+class MetadataCustom {
+  final String? who;
+  final String? what;
+
+  const MetadataCustom({this.who, this.what});
+}
