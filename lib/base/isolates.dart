1998f5347560f4e05da11a3ce0136370b0850c12
BlendAndroid <1059152860@qq.com>
2022-04-11 20:41:20 +0800

    feat：Dart基础--异步之事件队列和微队列

---
+++ lib/base/isolates.dart
@@ -0,0 +1,39 @@
+import 'dart:isolate';
+
+main() async {
+  var receivePort = new ReceivePort();
+  await Isolate.spawn(echo, receivePort.sendPort);
+
+  //'echo'发送的第一个message，是它的SendPort
+  //first 是 echo 线程的消息入口
+  var sendPort = await receivePort.first;
+
+  var msg = await sendReceive(sendPort, "foo");
+  print('received $msg');
+  msg = await sendReceive(sendPort, "bar");
+  print('received $msg');
+}
+
+/// 新isolate的入口函数
+echo(SendPort sendPort) async {
+  // 实例化一个ReceivePort 打开接收端口以接收消息
+  var port = new ReceivePort();
+
+  // 把它的sendPort发送给宿主isolate，以便宿主可以给它发送消息
+  sendPort.send(port.sendPort);
+
+  // 监听循环接收消息
+  await for (var msg in port) {
+    var data = msg[0];
+    SendPort replyTo = msg[1];
+    replyTo.send(data);
+    if (data == "bar") port.close();
+  }
+}
+
+/// 对某个port发送消息，并接收结果
+Future sendReceive(SendPort port, msg) {
+  ReceivePort response = new ReceivePort();
+  port.send([msg, response.sendPort]);
+  return response.first;
+}
